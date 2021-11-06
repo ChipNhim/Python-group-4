2bde159591fe17626571f0043149b62386dbd74a
sonnv <ngovietson1995@gmail.com>
2021-11-06 16:58:48 +0700

    Login_Logout

--- workcalendar/views.py
+++ workcalendar/views.py
@@ -1,8 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.contrib.auth import login, authenticate
+from django.contrib import messages
+from django.contrib.auth  import logout
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-def login(request):
-    return render(request, "layouts/login.html")
+def user_login(request):
+    if request.method == "POST":
+        email = request.POST.get("email", "")
+        password = request.POST.get("password", "")
+        user = authenticate(email = email, password = password)
 
+        if user is None:
+            my_message = (
+                "Your email address or password is incorrect, Pleasure login again!"
+            )
+            messages.error(request, my_message)
+            return render(request, "layouts/login.html")
+
+        #Luu lai cac section de sau khi dang nhap vao lan dau tien ma mo cac tab, trang khac se van su dung quyen cua tai khoan user
+        login(request, user) 
+        return HttpResponseRedirect("dashboard/")
+    return render(request, "layouts/login.html")
+@login_required
 def dashboard(request):
     return render(request, "dashboard/index.html")
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect("/")
